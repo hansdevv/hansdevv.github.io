f0947ae8c8a14526e8a5560cf207c1841d7a375f
mohamadburhan151 <mohamadburhan151@gmail.com>
2023-02-15 23:12:24 +0700

    perbaikan

--- include.js
+++ include.js
@@ -16,5 +16,5 @@ const  header = `
 `
 
 const libScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>'+
-+'<script src="assets/js/vendor/jquery-2.2.4.min.js"><script>'+
+'<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>'+
 '<script src="assets/js/functions-min.js"></script>';
