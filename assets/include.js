367b08d29733772f09ada07ecf09b51f30223475
mohamadburhan151 <mohamadburhan151@gmail.com>
2023-02-15 22:08:14 +0700

    add include js

---
+++ assets/include.js
@@ -0,0 +1,19 @@
+const  header = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Global</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="HTML5 website template">
+  <meta name="keywords" content="global, template, html, sass, jquery">
+  <meta name="author" content="Bucky Maler">
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+`
+
+const libScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>'+
++'<script src="assets/js/vendor/jquery-2.2.4.min.js"><script>'+
+'<script src="assets/js/functions-min.js"></script>';
